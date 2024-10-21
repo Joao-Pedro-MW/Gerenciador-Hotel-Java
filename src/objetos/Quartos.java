df75c525c176928611681facbfe3090f5e5839e8
João Pedro MW <jpedro01230@gmail.com>
2024-10-20 22:03:05 -0300

    Add class quartos

---
+++ src/objetos/Quartos.java
@@ -0,0 +1,22 @@
+package objetos;
+
+public class Quartos {
+	private int numero;
+	private String tipo;
+	private float preco;
+	private boolean disponibilidade;
+	
+	public Quartos(int numero, String tipo, float preco) {
+		this.numero = numero;
+		this.tipo = tipo;
+		this.preco = preco;
+		this.disponibilidade = true;
+	}
+
+	public int getNumero() {return numero;}
+	public String getTipo() {return tipo;}
+	public float getPreco() {return preco;}
+	public boolean getDisponibilidade() {return disponibilidade;}
+	public void setDisponibilidade(boolean disponibilidade) {this.disponibilidade = disponibilidade;}	
+	
+}
